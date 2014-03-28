75216d1924baabcd92884572d19ed748842ab808
Seth Denney <Seth.Denney@auburn.edu>
2014-03-28 13:21:22 -0500

    Fixed Box.h includes.

--- src/Metropolis/Box.h
+++ src/Metropolis/Box.h
@@ -13,8 +13,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "Utilitis/MathLibrary.h"
-#include "Utilities/Config_Scan.h"
+#include "Utilities/MathLibrary.h"
+#include "Metropolis/Utilities/IOUtilities.h"
 #include "Metropolis/DataTypes.h"
 #include "Utilities/StructLibrary.h"
 
