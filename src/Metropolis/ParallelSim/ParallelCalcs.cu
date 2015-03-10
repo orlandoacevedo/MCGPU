bf490e8823117c4b25b68ebea6d838873d06a5fa
yzd0014 <yzd0014@auburn.edu>
2015-03-09 22:13:42 -0500

    List here the changes you made

--- src/Metropolis/ParallelSim/ParallelCalcs.cu
+++ src/Metropolis/ParallelSim/ParallelCalcs.cu
@@ -186,8 +186,8 @@ Real ParallelCalcs::calcSystemEnergy(Box *box){
 	            cudaMemcpy(d_molecules, molecules, enviro->numOfMolecules*sizeof(Molecule), cudaMemcpyHostToDevice);
 	            cudaMemcpy(d_head, head, sizeof(int)*NCLMAX, cudaMemcpyHostToDevice);
 	            cudaMemcpy(d_lscl, lscl, sizeof(int)*NMAX, cudaMemcpyHostToDevice);
-
-	            ParallelCalcs::calcEnergy_NLC(d_molecules, d_enviro, d_head, d_lscl, d_totalEnergy);
+                
+	            calcEnergy_NLC<<dimGrid(lc[0], dimGrid[1], dimGrid[2]), dimBlock(3, 3, 3)>>(d_molecules, d_enviro, d_head, d_lscl, d_totalEnergy);
 				cudaMemcpy(&totalEnergy, d_totalEnergy, sizeof(Real), cudaMemcpyDeviceToHost);
 
 	            oldEnergy = totalEnergy + calcIntramolEnergy_NLC(enviro, molecules);
