1b185e7f05811d84d40242308191c8c57cf42fff
yzd0014 <yzd0014@auburn.edu>
2015-03-28 13:16:55 -0500

    test

--- src/Metropolis/ParallelSim/ParallelCalcs.cu
+++ src/Metropolis/ParallelSim/ParallelCalcs.cu
@@ -301,7 +301,7 @@ __global__ void ParallelCalcs::calcEnergy_NLC(Molecule *molecules, Environment *
 					// Calculate energy for entire molecule interaction if rij < Cutoff for atom index
 					if (rr < rrCut)
 					{	
-						part_energy[index] = 1;
+						//part_energy[index] = 1;
 					} /* Endif rr < rrCut */
 				} /* Endif i<j */
 				j = lscl[j];
