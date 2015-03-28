247b6822d12ea939677d06daedf7a8993dabd976
yzd0014 <yzd0014@auburn.edu>
2015-03-28 10:09:25 -0500

    test 8

--- src/Metropolis/ParallelSim/ParallelCalcs.cu
+++ src/Metropolis/ParallelSim/ParallelCalcs.cu
@@ -171,7 +171,7 @@ Real ParallelCalcs::calcSystemEnergy(Box *box){
 		head[c] = i;
 	} /* Endfor molecule i */
 
-	thrust::device_vector<Real> part_energy(lcxyz*27, 0);
+	thrust::device_vector<Real> part_energy(lcxyz*27, 1);
 	Real total_energy = 0;
 	Real oldEnergy;//result that will be returned
 
@@ -254,7 +254,7 @@ __global__ void ParallelCalcs::calcEnergy_NLC(Molecule *molecules, Environment *
 	id_z = 3*blockIdx.z + threadIdx.z;
 	index = id_z*lc[0]*lc[1] + id_y*lc[1] + id_x;
 
-	part_energy[index] = 1;//initialization
+	//part_energy[index] = 1;//initialization
 
 
 	/* Calculate pair interaction-----------------------------------------------*/
