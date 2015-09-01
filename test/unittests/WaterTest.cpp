b65cf03c9b3e62e76724acbc884c2f35f9e332ac
Mitchell Price <mcprice30@gmail.com>
2015-08-31 19:48:39 -0500

    Refactor Water Test.

--- test/unittests/WaterTest.cpp
+++ test/unittests/WaterTest.cpp
@@ -1,7 +1,7 @@
 
 #include "Applications/Application.h"
 #include "gtest/gtest.h"
-
+#include "TestUtil.h"
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
@@ -10,90 +10,37 @@
 #include <string>
 #include <cmath>
 
+/**
+ * Generates a configuration file to be used in testing.
+ * @param MCGPU_path The path to MCGPU's root directory.
+ * @param fileName The name of the configuration file to generate.
+ * @param primaryAtomIndexString The entry for the Primary Atom Index line of the config file.
+ */
+void createWaterConfigFile(std::string MCGPU, std::string fileName, std::string primaryAtomIndexString) {
+	ConfigFileData settings = ConfigFileData(55.0, 55.0, 55.0, 298.15, .15, 1000, 5120, "resources/bossFiles/oplsaa.par", 
+	"test/unittests/Integration/WaterTest/watt4p.z", "test/unittests/Integration/WaterTest", 25.0, 
+	15.0, 12345);
+	createConfigFile(MCGPU, fileName, primaryAtomIndexString, settings);
+}
+
 // Descr: evident
 // Implementation details: See gtest/samples for GTest syntax and usage
 TEST(WaterTest, FrontToEndIntegrationTest)
 {
-	string directory = get_current_dir_name();
-	
-	std::string mc ("MCGPU");
-	std::size_t found = directory.find(mc);
-	
-	if (found != std::string::npos) {
-		directory = directory.substr(0,found+6);
-	}
-	std::string MCGPU = directory;
-	
-	
-	// Create the test config file,
-	// 		because it requires full hardcoded filepath names that will change on each user's computer
-	
-	ofstream configFile;
-	std::string configFilePath (std::string( MCGPU + "/test/unittests/Integration/WaterTest/WaterTest.config"));
-	configFile.open( configFilePath.c_str() );
-	std::stringstream cfg;
-	cfg << ""
-		<< "#size of periodic box (x, y, z in angstroms)\n"
-		<< "55.0\n"
-		<< "55.0\n"
-		<< "55.0\n"
-		<< "#temperature in Kelvin\n"
-		<< "298.15\n"
-		<< "#max translation\n"
-		<< ".15\n"
-		<< "#number of steps\n"
-		<< "1000\n"
-		<< "#number of molecues\n"
-		<< "5120\n"
-		<< "#path to opls.par file\n"
-		<< MCGPU << "resources/bossFiles/oplsaa.par\n"
-		<< "#path to z matrix file\n"
-		<< MCGPU << "test/unittests/Integration/WaterTest/watt4p.z\n"
-		<< "#path to state input\n"
-		<< MCGPU << "test/unittests/Integration/WaterTest\n"
-		<< "#path to state output\n"
-		<< MCGPU << "test/unittests/Integration/WaterTest\n"
-		<< "#pdb output path\n"
-		<< "watertest.pdb\n"
-		<< "#cutoff distance in angstroms\n"
-		<< "25.0\n"
-		<< "#max rotation\n"
-		<< "15.0\n"
-		<< "#Random Seed Input\n"
-		<< "12345\n"
-		<< "#Primary Atom Index\n"
-		<< "1";
-	configFile << cfg.str();
-	configFile.close();
+	std::string MCGPU = getMCGPU_path();
+	createWaterConfigFile(MCGPU, "WaterTest.config", "1");
 	
     std::stringstream ss;    
 	ss << MCGPU << "/bin/metrosim "
        << " " // don't forget a space between the path and the arguments
        << MCGPU << "/test/unittests/Integration/WaterTest/WaterTest.config -s --name waterCPU -k";
 	
-	
     // Launch MCGPU application in serial, expect output files in /MCGPU/ directory   
 	system(ss.str().c_str());
 	
 	double expected = -14000;
-	double energyResult = -1;
+	double energyResult = getEnergyResult();
 	
-	std::ifstream infile( std::string(MCGPU + "bin/waterCPU.results").c_str() );
-	
-	for( std::string line; getline( infile, line ); )
-	{
-		std::string str2 ("Final-Energy");
-		std::string result;
-		found = line.find(str2);
-		
-		if (found != std::string::npos) {
-			result = line.substr(15);
-			energyResult  = strtod(result.c_str(), NULL);
-			break;
-		}
-	}
-	
-	// Clean up
 	remove(configFilePath.c_str());
 	remove(  std::string(MCGPU + "/waterCPU.pdb").c_str()  );
 	remove(  std::string(MCGPU + "/waterCPU.results").c_str()  );
